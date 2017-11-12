180552c6ff91559b0c0d6f6fe7c2c5406782c783
Prakash Gupta <prakash.pg.gupta@oracle.com>
2017-11-12 07:33:13 +0530

    New maven project

---
+++ projects/TestMaven/src/test/java/TestPlan1.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class TestPlan1 {
+  WebDriver driver = null;
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+	  System.setProperty("webdriver.ie.driver", "C:/IEDriverServer.exe");
+	  driver = new ChromeDriver();
+	  driver.get("http://newtours.demoaut.com");
+  }
+  
+  @Test
+  public void test1() {
+	  Assert.assertTrue(driver.findElement(By.linkText("Home")).isEnabled(), 
+				"Home link is not visible");
+  }
+  
+  @Test
+  public void test2() {
+	  Assert.assertFalse(driver.findElement(By.linkText("Home")).isEnabled(), 
+				"Home link is not visible");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.close();
+	  driver.quit();
+  }
+
+}
